988e8e3a306f4611e80250a27b747580d335193f
Igor Brigadir <igor.brigadir@gmail.com>
2020-05-07 23:34:47 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# covid19-twitter-stream-tool
-A tool to ingest the Twitter COVID-19 Labs Stream
+# Twitter Labs COVID-19 Stream Tools
+
+Get your Keys from you app in <https://developer.twitter.com/en/apps>
+
+Enable the Labs *COVID-19 stream* and *Labs' compliance firehose stream* with your app on <https://developer.twitter.com/en/account/labs>
+
+To start data gathering, run:
+
+```bash
+CONSUMER_KEY=xxx CONSUMER_SECRET=xxx docker-compose up -d
+```
+
+This will start a separate container for each stream partition, so that if one disconnects the rest will continue - hopefully this will minimise data loss. These will write separately into files, rotating and compressing every hour. This stream is roughly 8GB compressed per hour, but could be more, make sure you have disk space.
+
+You will need to re-assemble the stream for further processing together with the compliance stream, which is separated into 8 partitions. Compliance Stream is not implemented yet, I will add that and coe for merging the stream shortly.
+
+## See docker compose logs
+
+Use `docker-compose logs -f -t` to *attach* yourself to the logs of *all running partitions*, Use `Ctrl + z` or `Ctrl + c` to *detach* yourself from the log output *without* shutting down your running containers. If you're interested in logs of a single container you can use the `docker` keyword instead: `docker logs -t -f <name-of-service>`
+
+Tweets are saved in `/data` folder. You can change this in the docker-compose file if you want to mount somewhere else - this is where tweets will be written, in rotating hourly compressed `jsonl` files. Separately, if you also want to save the output of the docker compose logs to a file you can run: `docker-compose logs -f -t >> docker_compose_logs.log`.
---
+++ tweets.py
@@ -0,0 +1,102 @@
+import os
+import sys
+import logging.handlers
+import requests
+import time
+import subprocess
+
+
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+partition = os.environ.get("PARTITION_ID")
+
+if not consumer_key or not consumer_secret or not partition:
+    print("WARNING: Consumer Keys and Partition number are not set!")
+    sys.exit(0)
+
+
+def namer(name):
+    return f"{name}.jsonl"
+
+
+def rotator(source, dest):
+    print(f"Compressing {source} -> {dest}")
+    try:
+        os.rename(source, dest)
+        subprocess.Popen(["xz", dest])
+    except:
+        print(f"WARNING: FAILED TO COMPRESS {dest}")
+
+
+log_handler = logging.handlers.TimedRotatingFileHandler(
+    f"/data/covid19_partition_{partition}",
+    when="H",
+    interval=1,
+    encoding="utf-8",
+    utc=True,
+)
+log_handler.rotator = rotator
+log_handler.namer = namer
+
+logger = logging.getLogger("covid19_logger_{}".format(partition))
+logger.setLevel(logging.INFO)
+logger.addHandler(log_handler)
+
+
+def get_bearer_token(key, secret):
+    response = requests.post(
+        "https://api.twitter.com/oauth2/token",
+        auth=(key, secret),
+        data={"grant_type": "client_credentials"},
+        headers={
+            "User-Agent": "Python igorbrigadir/covid19-twitter-stream-tool v0.0.1"
+        },
+    )
+
+    if response.status_code is not 200:
+        raise Exception(
+            f"Cannot get a Bearer token (HTTP %d): %s"
+            % (response.status_code, response.text)
+        )
+
+    body = response.json()
+    return body["access_token"]
+
+
+def stream_connect(partition):
+    response = requests.get(
+        "https://api.twitter.com/labs/1/tweets/stream/covid19?partition={}".format(
+            partition
+        ),
+        headers={
+            "User-Agent": "Python igorbrigadir/covid19-twitter-stream-tool v0.0.1",
+            "Authorization": "Bearer {}".format(
+                get_bearer_token(consumer_key, consumer_secret)
+            ),
+        },
+        stream=True,
+    )
+    for response_line in response.iter_lines():
+        logger.info(response_line.decode("utf-8"))
+
+
+def main():
+    timeout = 0
+
+    while True:
+        print(f"Connecting to COVID-19 Stream Partition: {partition}")
+        try:
+            stream_connect(partition)
+            time.sleep(2 ** timeout * 1000)
+            timeout += 1
+        except:
+            pass
+        print(
+            f"WARNING: Disconnected Partition: {partition}, Attempt {timeout} to Reconnect"
+        )
+
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
