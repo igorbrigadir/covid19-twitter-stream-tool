7466a5cb1e9a572113dd0b6c14f5cd21cfd36b44
Igor Brigadir <igor.brigadir@gmail.com>
2020-05-18 21:04:51 +0100

    refactor, add better error handling, wait time should be seconds not milliseconds, drop connection and reconnect instead of keep_alive

--- src/stream-tweets/tweets.py
+++ src/stream-tweets/tweets.py
@@ -6,12 +6,15 @@ import time
 import subprocess
 
 
+user_agent = "Python igorbrigadir/covid19-twitter-stream-tool v0.0.3"
 consumer_key = os.environ.get("CONSUMER_KEY")
 consumer_secret = os.environ.get("CONSUMER_SECRET")
+stream_type = os.environ.get("STREAM_TYPE")
 partition = os.environ.get("PARTITION_ID")
 
-if not consumer_key or not consumer_secret or not partition:
-    print("WARNING: Consumer Keys and Partition number are not set!")
+
+if not consumer_key or not consumer_secret or not partition or not stream_type:
+    print("WARNING: Consumer Keys and Stream URL and Partition number are not set!")
     sys.exit(0)
 
 
@@ -28,8 +31,9 @@ def rotator(source, dest):
         print(f"WARNING: FAILED TO COMPRESS {dest}")
 
 
+folder = "compliance" if stream_type == "compliance" else "data"
 log_handler = logging.handlers.TimedRotatingFileHandler(
-    f"/data/covid19_partition_{partition}",
+    f"/{folder}/{stream_type}_partition_{partition}",
     when="H",
     interval=1,
     encoding="utf-8",
@@ -38,7 +42,7 @@ log_handler = logging.handlers.TimedRotatingFileHandler(
 log_handler.rotator = rotator
 log_handler.namer = namer
 
-logger = logging.getLogger("covid19_logger_{}".format(partition))
+logger = logging.getLogger(f"{stream_type}_logger_{partition}")
 logger.setLevel(logging.INFO)
 logger.addHandler(log_handler)
 
@@ -48,9 +52,7 @@ def get_bearer_token(key, secret):
         "https://api.twitter.com/oauth2/token",
         auth=(key, secret),
         data={"grant_type": "client_credentials"},
-        headers={
-            "User-Agent": "Python igorbrigadir/covid19-twitter-stream-tool v0.0.2"
-        },
+        headers={"User-Agent": user_agent},
     )
 
     if response.status_code is not 200:
@@ -64,36 +66,36 @@ def get_bearer_token(key, secret):
 
 
 def stream_connect(partition):
-    response = requests.get(
-        "https://api.twitter.com/labs/1/tweets/stream/covid19?partition={}".format(
-            partition
-        ),
-        headers={
-            "User-Agent": "Python igorbrigadir/covid19-twitter-stream-tool v0.0.2",
-            "Authorization": "Bearer {}".format(
-                get_bearer_token(consumer_key, consumer_secret)
-            ),
-        },
-        stream=True,
-    )
-    for response_line in response.iter_lines():
-        logger.info(response_line.decode("utf-8"))
+    with requests.session() as s:
+        s.keep_alive = False
+        response = s.get(
+            f"https://api.twitter.com/labs/1/tweets/stream/{stream_type}?partition={partition}",
+            headers={
+                "User-Agent": user_agent,
+                "Connection": "close",
+                "Authorization": f"Bearer {get_bearer_token(consumer_key, consumer_secret)}",
+            },
+            stream=True,
+        )
+        for response_line in response.iter_lines():
+            logger.info(response_line.decode("utf-8"))
 
 
 def main():
-    timeout = 0
+    retry = 0
 
     while True:
-        print(f"Connecting to COVID-19 Stream Partition: {partition}")
+        print(f"Connecting to {stream_type} Stream Partition: {partition}")
         try:
             stream_connect(partition)
-            time.sleep(2 ** timeout * 1000)
-            timeout += 1
+            wait_seconds = 2 ** retry
+            time.sleep(wait_seconds if wait_seconds < 900 else 900)
+            retry += 1
         except:
             pass
-        print(
-            f"WARNING: Disconnected Partition: {partition}, Attempt {timeout} to Reconnect"
-        )
+            print(
+                f"WARNING: Disconnected {stream_type} Partition: {partition}, Attempt {retry} to Reconnect"
+            )
 
     sys.exit(1)
 
