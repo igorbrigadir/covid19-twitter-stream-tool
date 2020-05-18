f9480dc8049b5a6e7fb5ce338456853c8674624c
Igor Brigadir <igor.brigadir@gmail.com>
2020-05-18 21:44:31 +0100

    add check script

---
+++ check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+printf "\nDocker:\n"
+docker ps
+printf "\n"
+docker-compose logs --tail="5"
+
+printf "\nHDD Space:\n"
+
+du -h data/
+du -h compliance/
+
+printf "\nData:\n"
+
+printf "Partition 1:"
+tail -n 1 data/covid19_partition_1 | jq '.text, .id_str'
+printf "Partition 2:"
+tail -n 1 data/covid19_partition_2 | jq '.text, .id_str'
+printf "Partition 3:"
+tail -n 1 data/covid19_partition_3 | jq '.text, .id_str'
+printf "Partition 4:"
+tail -n 1 data/covid19_partition_4 | jq '.text, .id_str'
