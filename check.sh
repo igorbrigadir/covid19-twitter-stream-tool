2d3d418440f555bdd043bcb81e00599468b217ef
Igor Brigadir <igor.brigadir@gmail.com>
2021-04-20 12:34:33 +0000

    add created at to status check.sh

--- check.sh
+++ check.sh
@@ -12,10 +12,10 @@ du -h compliance/
 printf "\nData:\n"
 
 printf "Partition 1:"
-tail -n 1 data/covid19_partition_1 | jq '.text, .id_str'
+tail -n 1 data/covid19_partition_1 | jq '.text, .id_str, .created_at'
 printf "Partition 2:"
-tail -n 1 data/covid19_partition_2 | jq '.text, .id_str'
+tail -n 1 data/covid19_partition_2 | jq '.text, .id_str, .created_at'
 printf "Partition 3:"
-tail -n 1 data/covid19_partition_3 | jq '.text, .id_str'
+tail -n 1 data/covid19_partition_3 | jq '.text, .id_str, .created_at'
 printf "Partition 4:"
-tail -n 1 data/covid19_partition_4 | jq '.text, .id_str'
+tail -n 1 data/covid19_partition_4 | jq '.text, .id_str, .created_at'
